ee99d49cb5f475c163871eaa243b1eee9ea75c3d
anitas-giraldo <anita.giraldo@marineecology.io>
2020-10-05 16:59:25 +0800

    commit

---
+++ Percent.cover.all.classe.methods.R
@@ -0,0 +1,375 @@
+####    Bar graph of % cover of benthic classes   ####
+
+
+#### Maps of all surveys in GB: BRUVS, AUV, FTV amd DTV --
+
+library(ggplot2)
+library(ggthemes)
+library(extrafont)
+library(broman) # for colors: https://kbroman.files.wordpress.com/2014/05/crayons.png
+library(raster)
+library(sp)
+library(sf)
+library(rgdal)
+library(plyr)
+library(maptools)
+#library(mapmisc)
+#install.packages("ggsn")
+#devtools::install_github("3wen/legendMap")
+library(ggsn)
+library(grid)
+library(maps)
+library(broom)
+library(tidyverse)
+library(dplyr)
+library(ggspatial)
+library(colorspace)
+
+## Clear workspace ----
+rm(list = ls())
+
+# Set working directory ####
+w.dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+#w.dir <- "H:/Github/GB_2015_Survey"
+# Set data directory - to read the data from
+d.dir <- paste(w.dir, "data", sep='/')
+s.dir <- (paste(w.dir, "shapefiles", sep='/'))
+# Set graph directory - to save plots
+p.dir <- paste(w.dir, "plots", sep='/')
+
+
+###  BRUV PLOT ----
+
+## read BRUV data ----
+bruv <- read.csv(paste(d.dir, "BRUVs_2014_CMR.csv", sep='/'))
+str(bruv)
+head(bruv)
+
+# Bruv to long format ----
+
+bruvlong <- gather(bruv, Class, measurement, Macroalgae:Consolidated, factor_key =T)
+head(bruvlong)
+str(bruvlong)
+levels(bruvlong$ZoneName)[levels(bruvlong$ZoneName)=="Special Purpose Zone (Mining Exclusion)"] <- "Special Purpose Zone"
+
+bav <- aggregate(measurement~Class+ZoneName, data = bruvlong, mean)
+# define SE function --
+se <- function(x) sd(x)/sqrt(length(x))
+bse <- aggregate(measurement~Class+ZoneName, data = bruvlong, se)
+head(bse)
+
+b <- cbind(bav, se = bse[,3])
+head(b)
+b$lower <- b$measurement-b$se
+b$upper <- b$measurement+b$se
+head(b)
+str(b)
+
+# reorder factors for plotting ----
+b$ZoneName <- ordered(b$ZoneName, levels=c("National Park Zone", "Habitat Protection Zone", "Multiple Use Zone", "Special Purpose Zone"))
+b$Class <- ordered(b$Class, levels = c("Seagrasses", "Unconsolidated", "Turf.algae", "Macroalgae", 
+                                       "Consolidated", "Sponges", "Stony.corals"))
+
+
+# Bruv colors ----
+bluepal <- choose_palette()
+greenpal <- choose_palette()
+
+# BRUV plot ----
+theme_set(theme_bw())
+pb <-ggplot(data=b, aes(x=Class, y=measurement, fill=Class)) +
+  geom_bar(stat="identity", color = "black") +
+  geom_errorbar(aes(ymin = measurement-se, ymax = measurement+se), width = 0.2, cex = 1) +
+  #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
+  facet_wrap(~ZoneName, ncol = 2, scales = 'free') +
+  #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
+  scale_fill_manual(values = greenpal(7)) +
+  labs(title = "Stereo-BRUVs", y = "Mean % cover") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_text(size = 12, face="bold"), 
+        axis.text.y = element_text(size = 12), 
+        axis.text.x = element_text(size=10, face="bold", color = "grey20", angle = 45, hjust = 1),
+        title = element_text(size = 14, face= "bold"),
+        strip.background = element_rect(fill = "grey90"),
+        strip.text = element_text(size = 10, color = "black", face ="bold"))
+
+pb
+
+
+
+
+###  FTV PLOT ----
+## read FTV sp ----
+ftv <- read.csv(paste(d.dir, "Ftv_CMR.csv", sep='/'))
+str(ftv)
+
+
+# Ftv to long format ----
+
+ftvlong <- gather(ftv, Class, measurement, Macroalgae:Consolidated, factor_key =T)
+head(ftvlong)
+str(ftvlong)
+levels(ftvlong$ZoneName)[levels(ftvlong$ZoneName)=="Special Purpose Zone (Mining Exclusion)"] <- "Special Purpose Zone"
+
+fav <- aggregate(measurement~Class+ZoneName, data = ftvlong, mean)
+# define SE function --
+se <- function(x) sd(x)/sqrt(length(x))
+fse <- aggregate(measurement~Class+ZoneName, data = ftvlong, se)
+head(fse)
+
+f <- cbind(fav, se = fse[,3])
+head(f)
+f$lower <- f$measurement-f$se
+f$upper <- f$measurement+f$se
+head(f)
+str(f)
+
+# reorder factors for plotting ----
+f$ZoneName <- ordered(f$ZoneName, levels=c("National Park Zone", "Habitat Protection Zone", "Multiple Use Zone", "Special Purpose Zone"))
+f$Class <- ordered(f$Class, levels = c("Seagrasses", "Unconsolidated", "Turf.algae", "Macroalgae", 
+                                       "Consolidated", "Sponges"))
+
+
+# FTV plot ----
+theme_set(theme_bw())
+pf <-ggplot(data=f, aes(x=Class, y=measurement, fill=Class)) +
+  geom_bar(stat="identity", color = "black") +
+  geom_errorbar(aes(ymin = measurement-se, ymax = measurement+se), width = 0.2, cex = 1) +
+  #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
+  facet_wrap(~ZoneName, ncol = 2, scales = 'free') +
+  #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
+  scale_fill_manual(values = greenpal(7)) +
+  labs(title = "Forward Towed Video", y = "Mean % cover") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_text(size = 12, face="bold"), 
+        axis.text.y = element_text(size = 12), 
+        axis.text.x = element_text(size=10, face="bold", color = "grey20", angle = 45, hjust = 1),
+        title = element_text(size = 14, face= "bold"),
+        strip.background = element_rect(fill = "grey90"),
+        strip.text = element_text(size = 10, color = "black", face ="bold"))
+
+pf
+
+
+
+
+
+
+
+#### AUV PLOT----
+## read AUV sp ----
+auv <- read.csv(paste(d.dir, "Auv_zoning.csv", sep='/'))
+str(auv)
+
+# Auv to long format ----
+
+along <- gather(auv, Class, measurement, Macroalgae:Consolidated, factor_key =T)
+head(along)
+str(along)
+levels(along$ZoneName)[levels(along$ZoneName)=="Special Purpose Zone (Mining Exclusion)"] <- "Special Purpose Zone"
+
+aav <- aggregate(measurement~Class+ZoneName, data = along, mean)
+# define SE function --
+se <- function(x) sd(x)/sqrt(length(x))
+ase <- aggregate(measurement~Class+ZoneName, data = along, se)
+head(ase)
+
+au <- cbind(aav, se = ase[,3])
+head(au)
+levels(au$ZoneName)
+str(au)
+
+# reorder factors for plotting ----
+au$ZoneName <- ordered(au$ZoneName, levels=c("National Park Zone", "Habitat Protection Zone", "Multiple Use Zone", "Special Purpose Zone"))
+au$Class <- ordered(au$Class, levels = c("Seagrasses", "Unconsolidated", "Turf.algae", "Macroalgae", 
+                                       "Consolidated", "Sponges", "Stony.corals"))
+
+
+# AUV plot ----
+theme_set(theme_bw())
+pa <-ggplot(data=au, aes(x=Class, y=measurement, fill=Class)) +
+  geom_bar(stat="identity", color = "black") +
+  geom_errorbar(aes(ymin = measurement-se, ymax = measurement+se), width = 0.2, cex = 1) +
+  #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
+  facet_wrap(~ZoneName, ncol = 2, scales = 'free') +
+  #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
+  scale_fill_manual(values = greenpal(7)) +
+  labs(title = "Autonomous Underwater Vehicle", y = "Mean % cover") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_text(size = 12, face="bold"), 
+        axis.text.y = element_text(size = 12), 
+        axis.text.x = element_text(size=10, face="bold", color = "grey20", angle = 45, hjust = 1),
+        title = element_text(size = 14, face= "bold"),
+        strip.background = element_rect(fill = "grey90"),
+        strip.text = element_text(size = 10, color = "black", face ="bold"))
+
+pa
+
+
+
+
+
+#### DTV PLOT ----
+## read DTV sp ----
+dtv <- read.csv(paste(d.dir, "Dtv_CMR.csv", sep='/'))
+str(dtv)
+
+# Dtv to long format ----
+
+dlong <- gather(dtv, Class, measurement, total.seagrass:Macroalgae, factor_key =T)
+head(dlong)
+str(dlong)
+levels(dlong$Zone)[levels(dlong$Zone)=="NPZ"] <- "National Park Zone"
+levels(dlong$Zone)[levels(dlong$Zone)=="HPZ"] <- "Habitat Protection Zone"
+levels(dlong$Zone)[levels(dlong$Zone)=="MUZ"] <- "Multiple Use Zone"
+levels(dlong$Zone)[levels(dlong$Zone)=="SPZ"] <- "Special Purpose Zone"
+
+dav <- aggregate(measurement~Class+Zone, data = dlong, mean)
+# define SE function --
+se <- function(x) sd(x)/sqrt(length(x))
+dse <- aggregate(measurement~Class+Zone, data = dlong, se)
+head(dse)
+
+d <- cbind(dav, se = dse[,3])
+head(d)
+levels(d$Zone)
+str(d)
+
+# reorder factors for plotting ----
+d$Zone <- ordered(d$ZoneName, levels=c("National Park Zone", "Habitat Protection Zone", "Multiple Use Zone", "Special Purpose Zone"))
+d$Class <- ordered(d$Class, levels = c("total.seagrass", "Unconsolidated", "Turf.algae", "Macroalgae", 
+                                         "Consolidated", "total.sponges", "corals", "other.inverts"))
+
+
+# AUV plot ----
+theme_set(theme_bw())
+pd <-ggplot(data=d, aes(x=Class, y=measurement, fill=Class)) +
+  geom_bar(stat="identity", color = "black") +
+  geom_errorbar(aes(ymin = measurement-se, ymax = measurement+se), width = 0.2, cex = 1) +
+  #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
+  facet_wrap(~Zone, ncol = 2, scales = 'free') +
+  #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
+  scale_fill_manual(values = greenpal(8)) +
+  labs(title = "Autonomous Underwater Vehicle", y = "Mean % cover") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_text(size = 12, face="bold"), 
+        axis.text.y = element_text(size = 12), 
+        axis.text.x = element_text(size=10, face="bold", color = "grey20", angle = 45, hjust = 1),
+        title = element_text(size = 14, face= "bold"),
+        strip.background = element_rect(fill = "grey90"),
+        strip.text = element_text(size = 10, color = "black", face ="bold"))
+
+pd
+
+
+## join all methods ----
+
+d # dtv
+d$Method <- "DTV"
+names(d) <- c("Class", "ZoneName"    ,    "measurement", "se" , "Method")
+au # auv
+au$Method <- "AUV"
+f # ftv
+f$Method <- "FTV"
+levels(f$ZoneName)[levels(f$ZoneName)=="Special Purpose Zone (Mining Exclusion)"] <- "Special Purpose Zone"
+b # bruvs
+head(b)
+b <- b[,c(1:4)]
+b$Method <- "Stereo-BRUVs"
+
+all <- rbind(d,au,b,f)
+head(all)
+str(all)
+all$Method <- as.factor(all$Method)
+
+# match names
+levels(all$Class)
+levels(all$Class)[levels(all$Class)=="total.seagrass"] <- "Seagrasses"
+levels(all$Class)[levels(all$Class)=="total.sponges"] <- "Sponges"
+levels(all$Class)[levels(all$Class)=="corals"] <- "Stony corals"
+levels(all$Class)[levels(all$Class)=="Stony.corals"] <- "Stony corals"
+levels(all$Class)[levels(all$Class)=="Turf.algae"] <- "Turf algae"
+
+# remove other 
+all <- all[all$Class!="other.inverts",]
+all <- droplevels(all)
+levels(all$Class)
+
+# change the zones
+all$ZoneName
+levels(all$ZoneName)[levels(all$ZoneName)=="Habitat Protection Zone"] <- "HPZ"
+levels(all$ZoneName)[levels(all$ZoneName)=="Multiple Use Zone"] <- "MUZ"
+levels(all$ZoneName)[levels(all$ZoneName)=="National Park Zone"] <- "NPZ"
+levels(all$ZoneName)[levels(all$ZoneName)=="Special Purpose Zone"] <- "SPZ"
+
+all$ZoneName <- ordered(all$ZoneName, levels=c("NPZ","HPZ", "MUZ", "SPZ"))
+
+head(all)
+str(all)
+# save ----
+#write.csv(all, paste(d.dir, "mean.class.cover.allmethods.csv", sep='/'))
+
+## To start plotting LOAD ALL DATA HERE ---
+all <- read.csv(paste(d.dir, "mean.class.cover.allmethods.csv", sep ='/'))
+
+# reorder factors for plotting ----
+all$Class <- ordered(all$Class, levels=c("Seagrasses", "Unconsolidated", "Turf algae", 
+                                         "Macroalgae", "Consolidated", "Sponges","Stony corals"))
+all$ZoneName <- ordered(all$ZoneName, levels=c("NPZ","HPZ", "MUZ", "SPZ"))
+all$Method <- ordered(all$Method, levels = c("Stereo-BRUVs",  "AUV" , "FTV", "DTV"))
+
+## color plot ---
+allpal <- c("#93dfb8" ,"#eceabe", "#80daeb",  "#dbd7d2")
+
+## plot all ----
+
+theme_set(theme_bw())
+pall <-ggplot(data=all, aes(x=Class, y=measurement, fill=ZoneName)) +
+  geom_bar(stat="identity", color = "black") +
+  geom_errorbar(aes(ymin = measurement-se, ymax = measurement+se), width = 0.2, cex = 1) +
+  #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
+  facet_grid(ZoneName~Method) +
+  #facet_wrap(~Zone, ncol = 2, scales = 'free') +
+  #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
+  scale_fill_manual(values = allpal) +
+  #labs(title = "Autonomous Underwater Vehicle", y = "Mean % cover") +
+  labs(y = "Mean % cover") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_text(size = 12, face="bold"), 
+        axis.text.y = element_text(size = 12), 
+        axis.text.x = element_text(size=10, face="bold", color = "grey20", angle = 45, hjust = 1),
+        title = element_text(size = 14, face= "bold"),
+        strip.background = element_rect(fill = "grey90"),
+        strip.text = element_text(size = 10, color = "black", face ="bold"))
+
+pall
+
+
+## plot all2 ----
+
+theme_set(theme_bw())
+pall2 <-ggplot(data=all, aes(x=Method, y=measurement, fill=ZoneName)) +
+  geom_bar(stat="identity", color = "black") +
+  geom_errorbar(aes(ymin = measurement-se, ymax = measurement+se), width = 0.2, cex = 1) +
+  #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
+  facet_grid(ZoneName~Class) +
+  #facet_wrap(~Zone, ncol = 2, scales = 'free') +
+  #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
+  scale_fill_manual(values = greenpal(7)) +
+  #labs(title = "Autonomous Underwater Vehicle", y = "Mean % cover") +
+  labs(y = "Mean % cover") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_text(size = 12, face="bold"), 
+        axis.text.y = element_text(size = 12), 
+        axis.text.x = element_text(size=10, face="bold", color = "grey20", angle = 45, hjust = 1),
+        title = element_text(size = 14, face= "bold"),
+        strip.background = element_rect(fill = "grey90"),
+        strip.text.x = element_text(size = 8, color = "black", face ="bold"),
+        strip.text.y = element_text(size = 10, color = "black", face ="bold"))
+
+pall2
+
+## save ----
+#ggsave("Cover.Method.Class.Zone.png", plot = pall2, path = p.dir, width = 200, height = 134, units = "mm", dpi = 300)
+ggsave("Cover.Method.Class.Zone.png", plot = pall2, path = p.dir, scale=1, dpi = 300)
+
--- Survey_Maps.R
+++ Survey_Maps.R
@@ -10,9 +10,16 @@ library(sf)
 library(rgdal)
 library(plyr)
 library(maptools)
+#library(mapmisc)
+#install.packages("ggsn")
+devtools::install_github("3wen/legendMap")
+library(ggsn)
+library(grid)
+library(maps)
 library(broom)
 library(tidyverse)
 library(dplyr)
+library(ggspatial)
 
 ## Clear workspace ----
 rm(list = ls())
@@ -31,21 +38,29 @@ p.dir <- paste(w.dir, "plots", sep='/')
 gb <- readOGR(paste(s.dir, "GeoBay.shp", sep='/'))
 proj4string(gb)
 
-## read WA shapefile ----
+## read GB coast shapefile ----
+#w <- readOGR(paste(s.dir, "WA_wgs84.shp", sep ='/'))
+#plot(w)
+#e <- drawExtent()
+#w <- crop(w,e)
+#plot(w)
+#plot(gb, add=T)
+#writeOGR(w, dsn = s.dir, "GB_coastline", driver = "ESRI Shapefile", overwrite_layer = T)
 coast <- readOGR(paste(s.dir, "GB_coastline.shp", sep='/'))
 proj4string(coast)
+plot(coast)
 
-## read BRUV sp ----
-#bruv <- read.csv(paste(d.dir, "BRUVs_2014_CMR.csv", sep='/'))
+## read BRUV data ----
+bruv <- read.csv(paste(d.dir, "BRUVs_2014_CMR.csv", sep='/'))
 #str(bruv)
 # make as BRUV sp points --
 #coordinates(bruv) <- ~coords.x1+coords.x2
 #proj4string(bruv) <- proj4string(coast)
 # save bruv shapefile --
 #writeOGR(bruv, s.dir, "Bruv_CMR", driver = "ESRI Shapefile")
-bruv <- readOGR(paste(s.dir, "Bruv_CMR.shp", sep='/'))
-plot(gb)
-plot(bruv, add=T)
+#bruv <- readOGR(paste(s.dir, "Bruv_CMR.shp", sep='/'))
+#plot(gb)
+#plot(bruv, add=T)
 
 
 ## read FTV sp ----
@@ -59,15 +74,21 @@ plot(bruv, add=T)
 #ftv2 <- crop(ftv, gb)
 #plot(gb)
 #points(ftv2)
+# save ftv as csv --
+#ftv3 <- as.data.frame(ftv2)
+#str(ftv3)
+#write.csv(ftv3, paste(d.dir, "Ftv_CMR.csv", sep='/'))
+ftv <- read.csv(paste(d.dir, "Ftv_CMR.csv", sep='/'))
+str(ftv)
 # save ftv CMR as shapefile --
 #writeOGR(ftv2, s.dir, "Ftv_CMR", driver = "ESRI Shapefile")
-ftv <- readOGR(paste(s.dir, "Ftv_CMR.shp", sep='/'))
-plot(gb)
-plot(ftv, add=T)
+#ftv <- readOGR(paste(s.dir, "Ftv_CMR.shp", sep='/'))
+#plot(gb)
+#plot(ftv, add=T)
 
 ## read AUV sp ----
-#auv <- read.csv(paste(d.dir, "Auv_zoning.csv", sep='/'))
-#str(auv)
+auv <- read.csv(paste(d.dir, "Auv_zoning.csv", sep='/'))
+str(auv)
 # make auv sp points --
 #coordinates(auv) <- ~coords.x1+coords.x2
 #proj4string(auv) <- proj4string(gb)
@@ -75,9 +96,9 @@ plot(ftv, add=T)
 #points(auv)
 # save AUV as shapefile --
 #writeOGR(auv, s.dir, "Auv_CMR", drive = "ESRI Shapefile")
-auv <- readOGR(paste(s.dir, "Auv_CMR.shp", sep='/'))
-plot(gb)
-plot(auv, add=T)
+#auv <- readOGR(paste(s.dir, "Auv_CMR.shp", sep='/'))
+#plot(gb)
+#plot(auv, add=T)
 
 
 ## read DTV sp ----
@@ -89,8 +110,86 @@ plot(auv, add=T)
 #plot(gb)
 #points(dtv)
 #dtv <- crop(dtv, gb)
+# save dtv as csv --
+#dtv2 <- as.data.frame(dtv)
+#str(dtv2)
+#write.csv(dtv2, paste(d.dir, "Dtv_CMR.csv", sep='/'))
+dtv <- read.csv(paste(d.dir, "Dtv_CMR.csv", sep='/'))
+str(dtv)
 # save dtv as shapefile --
 #writeOGR(dtv, s.dir, "Dtv_CMR", drive = "ESRI Shapefile")
-dtv <- readOGR(paste(s.dir, "Dtv_CMR.shp", sep='/'))
-plot(gb)
-plot(dtv, add=T)
+#dtv <- readOGR(paste(s.dir, "Dtv_CMR.shp", sep='/'))
+#plot(gb)
+#plot(dtv, add=T)
+
+
+#### Plot  ####
+
+# set colors #
+# for color list: https://kbroman.files.wordpress.com/2014/05/crayons.png
+grey <- brocolors("crayons")["Timberwolf"] # "#dbd7d2"
+green <- brocolors("crayons")["Spring Green"] # "#eceabe"
+blue <- brocolors("crayons")["Sky Blue"] # "#80daeb"
+green2 <- brocolors("crayons")["Sea Green"] # "#93dfb8"
+land <- brocolors("crayons")["Gold"] # "#deaa88"
+ocean <- brocolors("crayons")["Blizzard Blue"]
+
+## To plot the Geo Bay polygon ----
+gb@data$id = rownames(gb@data)
+gb.points = broom::tidy(gb)
+gb.df = join(gb.points, gb@data, by="id")
+class(gb.df)
+str(gb.df)
+levels(gb.df$ZoneName)
+levels(gb.df$ZoneName)[levels(gb.df$ZoneName)=="Special Purpose Zone (Mining Exclusion)"] <- "Special Purpose Zone"
+
+## To plot the coast polygon ----
+coast@data$id = rownames(coast@data)
+coast.points = broom::tidy(coast)
+coast.df = join(coast.points, coast@data, by="id")
+class(coast.df)
+str(coast.df)
+
+## plot ---
+
+p0 <- ggplot() +
+  geom_polygon(data = gb.df, aes(x = long, y = lat, group = group, fill = ZoneName), color = "black") +
+  geom_polygon(data = coast.df, aes(x = long, y = lat, group = group), fill = land, color = "black") +
+  #geom_point(aes(x=coords.x1, y=coords.x2), data=bruv) +
+  #geom_path(color = "white", size = 0.2) +
+  #scale_fill_gradient(breaks=c(0.33,0.66,0.99), labels=c("Low","Medium","High")) + 
+  coord_equal(ratio= 1) +
+  #xlab("Latitude") + ylab("Longitude") +
+  scale_fill_manual("Zones", values = c("#eceabe", "#80daeb", "#93dfb8" , "#dbd7d2"), guide=guide_legend(nrow=4, title.position = 'top'))+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black")) +
+  theme(axis.title = element_blank()) +
+  #theme(legend.position = "bottom", legend.box = "vertical", legend.title = element_text(face="bold")) +
+  #labs(title = "Geographe Bay") +
+  xlab("Latitude") + ylab("Longitude") +
+  #xlim(114.95, 115.65) + ylim( -33.7, -33.25 ) +
+  #geom_point(aes(x=coords.x1, y=coords.x2, color = bruv$ZoneName), data=bruv,alpha=1, size=3, color="grey20")+ # to get outline
+  #add the points from surveys --
+  # BRUV --
+  geom_point(aes(x=coords.x1, y=coords.x2, color = "Stereo-BRUVs"), data=bruv,alpha=1, size=1.5) +
+  # AUV --
+  geom_point(aes(x=coords.x1, y=coords.x2, color = "AUV grids"), data=auv,alpha=1, size=1.5) +
+  # FTV --
+  geom_point(aes(x=coords.x1, y=coords.x2, color = "Forward Towed Video transects"), data=ftv,alpha=1, size=1.5) +
+  # DTV --
+  geom_point(aes(x=Longitude, y=Latitude, color = "Downward towed Video transects"), data=dtv,alpha=1, size=1.5) +
+  scale_colour_manual("Benthic survey methods", values = c("blue", "red","green3", "black"), guide=guide_legend(nrow=4, title.position = "top")) +  # change color scale
+  theme(legend.position = "bottom", legend.box = "horizontal", legend.title = element_text(face="bold"),
+        panel.background = element_rect(fill = "lightblue1"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  annotation_north_arrow(which_north = 'grid', height = unit(1.5, "cm"), width = unit(1.5, "cm"), pad_x = unit(15.25, "cm"),
+                         pad_y = unit(0.59, "cm")) 
+  #annotation_scale(plot_unit = 'km')
+  #ggsn::scalebar(gb, location = "bottomright", dist = 2, st.size = 10, height = 0.05, transform = TRUE, dist_unit= 'km', model = 'GRS80', label = '10 km', 
+  #               x.min = 115.0 , x.max = 115.6 , y.min = -33.69, y.max = -33.35)                                                                                                            
+  #xlab("Latitude") + ylab("Longitude") 
+
+p0
+
+ggsave("GB-All-surveys.png", plot = p0, path = p.dir, width = 200, height = 134, units = "mm", dpi = 300)
+

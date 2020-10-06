b29fb52fccc0be59c42cfbb75b5c70033fb79651
anitas-giraldo <anita.giraldo@marineecology.io>
2020-10-06 17:03:16 +0800

    commit

--- Seagrass.grid.transect.zone.R
+++ Seagrass.grid.transect.zone.R
@@ -253,16 +253,21 @@ levels(tsg2$ZoneName)[levels(tsg2$ZoneName)=="Special Purpose Zone"] <- "SPZ"
 # reorder factors for plotting ----
 tsg2$ZoneName <- ordered(tsg2$ZoneName, levels=c("NPZ", "HPZ", "MUZ", "SPZ"))
 
+# remove Thalassodendron
+tsg2 <- tsg2[tsg2$Seagrass!="Thalassodendrum",]
+levels(tsg2$Seagrass)
+tsg2 <- droplevels(tsg2)
+             
 # plot FTV seagrass ----
 
 # FTV colors ----
-bluepal <- choose_palette()
-greenpal <- choose_palette()
-zonecolors <- c("#93dfb8" , "#eceabe", "#80daeb", "#dbd7d2")
+#bluepal <- choose_palette()
+#greenpal <- choose_palette()
+#zonecolors <- c("#93dfb8" , "#eceabe", "#80daeb", "#dbd7d2")
 
 
 theme_set(theme_bw())
-pf <-ggplot(data=bsg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
+pf <-ggplot(data=tsg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
   geom_bar(stat="identity", color = "black") +
   geom_errorbar(aes(ymin = Mean-SE, ymax = Mean+SE), width = 0.2, cex = 1) +
   #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
@@ -387,17 +392,21 @@ levels(asg2$Seagrass)
 # reorder factors for plotting ----
 asg2$ZoneName <- ordered(asg2$ZoneName, levels=c("NPZ", "HPZ", "MUZ", "SPZ"))
 
+# remove levels
+levels(asg2$Seagrass)
+asg2 <- asg2[asg2$Seagrass!="Rupia",]
+asg2 <- droplevels(asg2)
 
 # plot FTV seagrass ----
 
 # AuV colors ----
-bluepal <- choose_palette()
-greenpal <- choose_palette()
-zonecolors <- c("#93dfb8" , "#eceabe", "#80daeb", "#dbd7d2")
+#bluepal <- choose_palette()
+#greenpal <- choose_palette()
+#zonecolors <- c("#93dfb8" , "#eceabe", "#80daeb", "#dbd7d2")
 
 
 theme_set(theme_bw())
-pa <-ggplot(data=bsg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
+pa <-ggplot(data=asg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
   geom_bar(stat="identity", color = "black") +
   geom_errorbar(aes(ymin = Mean-SE, ymax = Mean+SE), width = 0.2, cex = 1) +
   #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
@@ -422,9 +431,9 @@ ggsave("Seagrass.AUV.png", plot = pa, path = p.dir, scale=1, dpi = 300)
 
 ## DTV ####
 # ## read BRUV data ----
-dtv <- read.csv(paste(d.dir, "DTV_GeoBay_May2020_percent.cover.n.metadata.csv", sep='/'))
+dtv <- read.csv(paste(d.dir, "DTV-habitat-percent-cover-detailed-06102020.csv", sep='/'))
 str(dtv)
-dtv$Latitude <- dtv$Latitude*(-1)
+#dtv$Latitude <- dtv$Latitude*(-1)
 head(dtv)
 # create id column
 #id <- seq(1:2962)
@@ -453,11 +462,13 @@ dtv <- cbind(dsp, dsp2)
 str(dtv)
 names(dtv)
 # remove unnecessary columns --
-dtv <- dtv[,c(1,33,35,36,40,43,61,62,63,64,65)]
+dtv <- dtv[,c(2:9,17,18,21,29)]
 head(dtv)
-dtv$unk.strap.like <- dtv$Seagrasses.Strap.Lik.Leaves-
-  (dtv$Seagrasses.Strap.Lik.Leaves.Amphiboli.sp.+dtv$Seagrasses.Strap.Lik.Leaves.Posidoni.sp.+
-     dtv$ Seagrasses.Strap.Lik.Leaves.Rupi.sp..epiphytes.algae)
+
+### UP TO HERE
+dtv$unk.strap.like <- dtv$total.seagrass-
+  (dtv$Amphibolis+dtv$Posidonia+
+     dtv$Halophila)
 head(dtv)
 levels(dtv$ZoneName)[levels(dtv$ZoneName)=="Special Purpose Zone (Mining Exclusion)"] <- "Special Purpose Zone"
 head(dtv)
@@ -467,55 +478,55 @@ head(dtv)
 # se function --
 se <- function(x) sd(x)/sqrt(length(x))
 # amphibolis --
-amp <- aggregate(Amphibolis~ZoneName, data = ftv, mean)
-ampse <- aggregate(Amphibolis~ZoneName, data = ftv, se)
+amp <- aggregate(Amphibolis~ZoneName, data = dtv, mean)
+ampse <- aggregate(Amphibolis~ZoneName, data = dtv, se)
 # posidonia --
-pos <- aggregate(Posidonia~ZoneName, data = ftv, mean)
-pose <- aggregate(Posidonia~ZoneName, data = ftv, se)
+pos <- aggregate(Posidonia~ZoneName, data = dtv, mean)
+pose <- aggregate(Posidonia~ZoneName, data = dtv, se)
 # Zostera --
-th <- aggregate(Thalassodendrum~ZoneName, data = ftv, mean)
-thse <- aggregate(Thalassodendrum~ZoneName, data = ftv, se)
+h <- aggregate(Halophila~ZoneName, data = dtv, mean)
+hse <- aggregate(Halophila~ZoneName, data = dtv, se)
 # unknown strap --
-unk <- aggregate(unk.strap.like~ZoneName, data = ftv, mean)
-unkse <- aggregate(unk.strap.like~ZoneName, data = ftv, se)
+unk <- aggregate(unk.strap.like~ZoneName, data = dtv, mean)
+unkse <- aggregate(unk.strap.like~ZoneName, data = dtv, se)
 
 # join -- 
-tsg <- cbind(amp, pos, th,  unk)
-head(tsg )
-names(tsg )
-tsg  <- tsg [,c(1,2,4,6,8)]
-head(tsg )
+dsg <- cbind(amp, pos, h,  unk)
+head(dsg )
+names(dsg)
+dsg  <- dsg [,c(1,2,4,6,8)]
+head(dsg )
 
-tsg1 <- cbind(ampse, pose, thse,  unkse)
-head(tsg1)
-names(tsg1)
-tsg1 <- tsg1[,c(1,2,4,6,8)]
-head(tsg1)
-names(tsg1) <- c("ZoneName",  "AmphibolisSE","PosidoniaSE",  "ThalassoSE" ,    "unk.strap.like.SE")
+dsg1 <- cbind(ampse, pose, hse,  unkse)
+head(dsg1)
+names(dsg1)
+dsg1 <- dsg1[,c(1,2,4,6,8)]
+head(dsg1)
+names(dsg1) <- c("ZoneName",  "AmphibolisSE","PosidoniaSE",  "HalophilaSE" ,    "unk.strap.like.SE")
 
 # make long ---
-tsg <- melt(tsg, od.vars=c("ZoneName"))
-head(tsg)
-names(tsg) <- c("ZoneName", "Seagrass", "Mean")
+dsg <- melt(dsg, od.vars=c("ZoneName"))
+head(dsg)
+names(dsg) <- c("ZoneName", "Seagrass", "Mean")
 
-tsg1 <- melt(tsg1, od.vars=c("ZoneName"))
-head(tsg1)
-names(tsg1) <- c("ZoneName", "Seagrass", "SE")
+dsg1 <- melt(dsg1, od.vars=c("ZoneName"))
+head(dsg1)
+names(dsg1) <- c("ZoneName", "Seagrass", "SE")
 
-tsg2 <- cbind(tsg, tsg1)
-head(tsg2)
-tsg2 <- tsg2[,c(1,2,3,6)]
-head(tsg2)
-str(tsg2)
+dsg2 <- cbind(dsg, dsg1)
+head(dsg2)
+dsg2 <- dsg2[,c(1,2,3,6)]
+head(dsg2)
+str(dsg2)
 # rename factors ----
-levels(tsg2$Seagrass)[levels(tsg2$Seagrass)=="unk.strap.like"] <- "Unidentified strap-like"
-levels(tsg2$ZoneName)[levels(tsg2$ZoneName)=="National Park Zone"] <- "NPZ"
-levels(tsg2$ZoneName)[levels(tsg2$ZoneName)=="Habitat Protection Zone"] <- "HPZ"
-levels(tsg2$ZoneName)[levels(tsg2$ZoneName)=="Multiple Use Zone"] <- "MUZ"
-levels(tsg2$ZoneName)[levels(tsg2$ZoneName)=="Special Purpose Zone"] <- "SPZ"
+levels(dsg2$Seagrass)[levels(dsg2$Seagrass)=="unk.strap.like"] <- "Unidentified strap-like"
+levels(dsg2$ZoneName)[levels(dsg2$ZoneName)=="National Park Zone"] <- "NPZ"
+levels(dsg2$ZoneName)[levels(dsg2$ZoneName)=="Habitat Protection Zone"] <- "HPZ"
+levels(dsg2$ZoneName)[levels(dsg2$ZoneName)=="Multiple Use Zone"] <- "MUZ"
+levels(dsg2$ZoneName)[levels(dsg2$ZoneName)=="Special Purpose Zone"] <- "SPZ"
 
 # reorder factors for plotting ----
-tsg2$ZoneName <- ordered(tsg2$ZoneName, levels=c("NPZ", "HPZ", "MUZ", "SPZ"))
+dsg2$ZoneName <- ordered(dsg2$ZoneName, levels=c("NPZ", "HPZ", "MUZ", "SPZ"))
 
 # plot FTV seagrass ----
 
@@ -526,7 +537,7 @@ zonecolors <- c("#93dfb8" , "#eceabe", "#80daeb", "#dbd7d2")
 
 
 theme_set(theme_bw())
-pf <-ggplot(data=bsg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
+pd <-ggplot(data=dsg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
   geom_bar(stat="identity", color = "black") +
   geom_errorbar(aes(ymin = Mean-SE, ymax = Mean+SE), width = 0.2, cex = 1) +
   #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
@@ -534,7 +545,7 @@ pf <-ggplot(data=bsg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
   #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
   #scale_fill_manual(values = greenpal(4)) +
   scale_fill_manual(values = zonecolors) +
-  labs(title = "FTV", y = "Mean % cover") +
+  labs(title = "DTV", y = "Mean % cover") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
         axis.title.x = element_blank(), axis.title.y = element_text(size = 14, face="bold"), 
         axis.text.y = element_text(size = 14), 
@@ -544,6 +555,59 @@ pf <-ggplot(data=bsg2, aes(x=ZoneName, y=Mean, fill=ZoneName)) +
         strip.text.x = element_text(size = 14, color = "black", face ="bold"),
         strip.text.y = element_text(size = 14, color = "black", face ="bold"))
 
-pf
+pd
 
-ggsave("Seagrass.FTV.png", plot = pf, path = p.dir, scale=1, dpi = 300)
+ggsave("Seagrass.DTV.png", plot = pd, path = p.dir, scale=1, dpi = 300)
+
+
+### All methods  -----
+
+head(bsg2)
+bsg2$Method <- "Stereo-BRUVs"
+head(tsg2)
+tsg2$Method <- "FTV"
+head(asg2)
+asg2$Method <- "AUV"
+head(dsg2)
+dsg2$Method <- "DTV"
+
+# join 
+all <- rbind(bsg2, tsg2, asg2, dsg2)
+all
+
+# save this data --
+#write.csv(all, paste(d.dir, "all.seagrass.all.methods.csv", sep='/'))
+
+
+## plot all ----
+bluepal <- choose_palette()
+greenpal2 <- choose_palette()
+
+levels(all$Seagrass)
+all <- all[all$Seagrass!="Zostera",]
+all <- all[all$Seagrass!="Halophila",]
+
+theme_set(theme_bw())
+pall <-ggplot(data=all, aes(x=Method, y=Mean, fill=ZoneName)) +
+  geom_bar(stat="identity", color = "black") +
+  geom_errorbar(aes(ymin = Mean-SE, ymax = Mean+SE), width = 0.2, cex = 1) +
+  #geom_errorbar(aes(ymax = mean-sd, ymin = mean+sd), width = 0.2, color = "blue") +
+  facet_grid(ZoneName~Seagrass) +
+  #facet_wrap(~Zone, ncol = 2, scales = 'free') +
+  #scale_fill_manual(values = c("#77dde7", "#fc6c85","#b2ec5d", "#ffcf48")) +
+  scale_fill_manual(values = greenpal2(5)) +
+  #labs(title = "Autonomous Underwater Vehicle", y = "Mean % cover") +
+  labs(y = "Mean % cover") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_text(size = 12, face="bold"), 
+        axis.text.y = element_text(size = 12), 
+        axis.text.x = element_text(size=10, face="bold", color = "grey20", angle = 45, hjust = 1),
+        title = element_text(size = 14, face= "bold"),
+        strip.background = element_rect(fill = "grey90"),
+        strip.text.x = element_text(size = 8, color = "black", face ="bold"),
+        strip.text.y = element_text(size = 10, color = "black", face ="bold"))
+
+pall
+
+
+ggsave("Seagrass2.png", plot = pall, path = p.dir, scale=1, dpi = 300)

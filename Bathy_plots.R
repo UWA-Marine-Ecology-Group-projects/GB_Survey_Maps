28db0baa07769d803c1f144f65dbb446cec920d9
anitas-giraldo <anita.giraldo@marineecology.io>
2020-10-19 08:15:34 +0800

    commit

--- Bathy_plots.R
+++ Bathy_plots.R
@@ -39,6 +39,7 @@ library(patchwork)
 #install.packages("corrplot")
 #library(corrplot)
 library(broman)
+library(viridis)
 
 
 # Clear memory ----
@@ -48,7 +49,7 @@ rm(list=ls())
 w.dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 d.dir <- paste(w.dir, "data", sep='/')
 s.dir <- paste(w.dir, "shapefiles", sep='/')
-r.dir <- paste(w.dir, "raster", sep='/')
+r.dir <- paste(w.dir, "rasters", sep='/')
 p.dir <- paste(w.dir, "plots", sep='/')
 
 
@@ -59,6 +60,7 @@ p.dir <- paste(w.dir, "plots", sep='/')
 # Read gb cmr poly ----
 gb <- readOGR(paste(s.dir, "GeoBay.shp",sep='/'))
 plot(gb)
+crs1 <- proj4string(gb)
 levels(gb$ZoneName)
 # get poly for each zone --
 NPZ <- gb[gb$ZoneName=="National Park Zone",]
@@ -75,46 +77,149 @@ pal1 <- c(sand, sg, alg )
 
 
 
-
-#### ####     FINE BATHY PREDICTIONS     #### 
-
-####    BRUVs Fine    ####
+### MBES data ----
 
 # Read data ----
-pred <- raster(paste(o.dir, "GBpred-Fine-Bruvs.tif", sep='/'))
-plot(pred)
+b <- raster(paste(r.dir, "multibeamGB_UTM.tif", sep='/'))
+plot(b)
+proj4string(b)
+b2 <- projectRaster(b, crs = crs1)
 
-# raster as data frame --
-#redf <- as.data.frame(pred, xy=TRUE) %>% na.omit()
-#head(predf)
 
-# fix class levels for plotting --
-xx <-levels(pred)[[1]]
-xx
-class(xx)
-xx <- xx[-1,]
-xx$ID <- xx$ID[xx$ID != "0",]
-xx$ID <- c('3','2', '1')
-xx$category <- c('Unconsolidated',  'Seagrasses', 'Algae' )
-levels(pred) <- xx
-pred
-#xx <-levels(pred)[[1]]
 
 ## Plot using tmap ----
 # https://geocompr.robinlovelace.net/adv-map.html
 
-
-map1 <- tm_shape(pred) + tm_raster(palette=pal1)
-
-map2 <- map1 + tm_shape(gb)  + tm_borders(col ='black', lwd = 2) +
-  tm_compass(type = "arrow", position = c(0.84, 0.25), size = 4) +
+map <- tm_shape(gb)  + tm_borders(col ='black', lwd = 2) +
+  tm_compass(type = "arrow", position = c(0.9, 0.59), size = 4) +
   tm_scale_bar(breaks = c(0, 5, 10), text.size = 1) + 
-  tm_layout(legend.text.size = 1,
-            legend.position = c(0.76, 0.1),
-            legend.title.color = 'white') +
   #tm_graticules(ticks = FALSE) +
   tm_grid(n.x = 3, n.y = 3, labels.size = 1.5, lines = FALSE) 
+map
+
+map1 <- map + tm_shape(b2) + tm_raster(palette=viridis(40, direction =-1), style = 'cont', legend.reverse = TRUE) +
+  tm_layout(legend.text.size = 1.7,
+            legend.position = c(0.85, 0.15),
+            legend.title.color = 'white') + map
+map1
 
 
-map2
-class(map2)
+## save map ----
+
+tmap_save(map1, paste(p.dir, "GB-MBES.tiff", sep='/'))
+
+
+
+
+### LIDAR data ----
+
+# Read data ----
+b <- raster(paste(r.dir, "lidarGB_all_UTM.tif", sep='/'))
+plot(b)
+proj4string(b)
+
+b2 <- projectRaster(b, crs = crs1)
+plot(b2)
+plot(gb, add = T)
+e <- drawExtent()
+b3 <- crop(b2, e)
+plot(b3)
+plot(gb, add=T)
+
+## Plot using tmap ----
+# https://geocompr.robinlovelace.net/adv-map.html
+
+map <- tm_shape(gb)  + tm_borders(col ='black', lwd = 2) +
+  tm_compass(type = "arrow", position = c(0.9, 0.45), size = 4) +
+  tm_scale_bar(breaks = c(0, 5, 10), text.size = 1) + 
+  #tm_graticules(ticks = FALSE) +
+  tm_grid(n.x = 3, n.y = 3, labels.size = 1.5, lines = FALSE) 
+map
+
+map1 <- tm_shape(b3) + tm_raster(palette=viridis(40, direction =-1), style = 'cont', legend.reverse = TRUE) +
+  tm_layout(legend.text.size = 1.7,
+            legend.position = c(0.82, 0.1),
+            legend.title.color = 'white',
+            legend.title.size = 0.11) + map
+map1
+
+
+## save map ----
+
+tmap_save(map1, paste(p.dir, "GB-LIDAR.tiff", sep='/'))
+
+
+
+### MBES and LIDAR data ----
+
+# Read data ----
+b <- raster(paste(r.dir, "GBmultib_lidar_CMR.tif", sep='/'))
+plot(b)
+proj4string(b)
+
+plot(gb, add = T)
+e <- drawExtent()
+b3 <- crop(b, e)
+plot(b3)
+plot(gb, add=T)
+
+## Plot using tmap ----
+# https://geocompr.robinlovelace.net/adv-map.html
+
+map <- tm_shape(gb)  + tm_borders(col ='black', lwd = 2) +
+  tm_compass(type = "arrow", position = c(0.9, 0.59), size = 4) +
+  tm_scale_bar(breaks = c(0, 5, 10), text.size = 1) + 
+  #tm_graticules(ticks = FALSE) +
+  tm_grid(n.x = 3, n.y = 3, labels.size = 1.5, lines = FALSE) 
+map
+
+map1 <- tm_shape(b3) + tm_raster(palette=viridis(40, direction =-1), style = 'cont', legend.reverse = TRUE) +
+  tm_layout(legend.text.size = 1.7,
+            legend.position = c(0.82, 0.15),
+            legend.title.color = 'white') + map
+map1
+
+
+## save map ----
+
+tmap_save(map1, paste(p.dir, "GB-Fine-bathy.tiff", sep='/'))
+
+
+
+### COARSE data ----
+
+# Read data ----
+b <- raster(paste(r.dir, "Geog_250mBathy.tif", sep='/'))
+plot(b)
+proj4string(b)
+
+b2 <-  raster(paste(r.dir, "GB-SW_250mBathy.tif", sep='/'))
+plot(b2)
+plot(gb, add = T)
+e <- drawExtent()
+b3 <- crop(b2, e)
+plot(b3)
+plot(gb, add=T)
+
+## Plot using tmap ----
+# https://geocompr.robinlovelace.net/adv-map.html
+
+map <- tm_shape(gb)  + tm_borders(col ='black', lwd = 2) +
+  tm_compass(type = "arrow", position = c(0.9, 0.45), size = 4) +
+  tm_scale_bar(breaks = c(0, 5, 10), text.size = 1) + 
+  #tm_graticules(ticks = FALSE) +
+  tm_grid(n.x = 3, n.y = 3, labels.size = 1.5, lines = FALSE) 
+map
+
+map1 <- tm_shape(b) + tm_raster(palette=viridis(40, direction =-1), style = 'cont', legend.reverse = TRUE) +
+  tm_layout(legend.text.size = 1.7,
+            legend.position = c(0.78, 0.15),
+            legend.title.color = 'white') + map
+map1
+
+
+## save map ----
+
+tmap_save(map1, paste(p.dir, "GB-coarse-bathy_CMR.tiff", sep='/'))
+
+
